1aad28af12fdaedb04e528bd9d8d0d593cb6075e
Max Mazur <maxmen43@gmail.com>
2016-11-02 16:28:29 +0200

    slide up down pannel

---
+++ js/jquery.js
@@ -0,0 +1,9 @@
+/**
+ * Created by 777 on 02.11.2016.
+ */
+$(document).ready(function () {
+    $("#player").hide();
+    $("#subbut").click(function () {
+        $("#player").show("slow");
+    })
+})
